411935cc65b1ccdadeb913d31511bd45eb1c34e2
vitorbland <144287893+vitorbland@users.noreply.github.com>
2024-06-17 22:35:09 -0300

    Update Garagem.java

--- Trabalho2Bim/src/Garagem.java
+++ Trabalho2Bim/src/Garagem.java
@@ -3,12 +3,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Garagem {
-    List<Veiculo> veiculos;
-    List<Pessoa> pessoas;
+	ArrayList<Veiculo> veiculos=new ArrayList<>();
+
 
     public Garagem() {
         veiculos = new ArrayList<>();
-        pessoas = new ArrayList<>();
+  
     }
 
     public void adicionarVeiculo(Veiculo veiculo) {
@@ -19,35 +19,10 @@ public class Garagem {
         veiculos.remove(veiculo);
     }
 
-    public void adicionarPessoa(Pessoa pessoa) {
-        pessoas.add(pessoa);
-    }
-
-    public void removerPessoa(Pessoa pessoa) {
-        pessoas.remove(pessoa);
-    }
-
     public void listarVeiculos() {
         for (int i = 0; i < veiculos.size(); i++) {
             System.out.println(i + ". " + veiculos.get(i).getMarca() + " " + veiculos.get(i).getModelo() + " (" + veiculos.get(i).getAno() + ")");
         }
-    }
-
-    public void listarPessoas() {
-        for (int i = 0; i < pessoas.size(); i++) {
-            System.out.println(i + ". " + pessoas.get(i).getNome() + " - CPF: " + pessoas.get(i).getClass());
-        }
-    }
-
-    public void visualizarDetalhesVeiculo(Veiculo veiculo) {
-        veiculo.visualizarDetalhes();
-    }
-
-    public void visualizarDetalhesPessoa(Pessoa pessoa) {
-        System.out.println("Nome: " + pessoa.getNome());
-        System.out.println("CPF: " + pessoa.getCpf());
-        System.out.println("Idade: " + pessoa.getIdade());
-        // Adicione mais detalhes se necessário
     }
 
     public void excluirVeiculo(int indice) {
@@ -56,15 +31,6 @@ public class Garagem {
             System.out.println("Veículo removido: " + veiculoRemovido.getMarca() + " " + veiculoRemovido.getModelo());
         } else {
             System.out.println("Índice inválido! Veículo não removido.");
-        }
-    }
-
-    public void excluirPessoa(int indice) {
-        if (indice >= 0 && indice < pessoas.size()) {
-            Pessoa pessoaRemovida = pessoas.remove(indice);
-            System.out.println("Pessoa removida: " + pessoaRemovida.getNome());
-        } else {
-            System.out.println("Índice inválido! Pessoa não removida.");
         }
     }
 
@@ -77,12 +43,4 @@ public class Garagem {
         }
     }
 
-    public void editarPessoa(int indice, Pessoa novaPessoa) {
-        if (indice >= 0 && indice < pessoas.size()) {
-            pessoas.set(indice, novaPessoa);
-            System.out.println("Pessoa editada com sucesso!");
-        } else {
-            System.out.println("Índice inválido! Pessoa não editada.");
-        }
-    }
 }
